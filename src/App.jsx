38b77ce8e3e36c55c16c9cb01bce8a8cb45e49de
namang48 <namangupta.mp4@gmail.com>
2024-11-30 00:40:59 +0530

    change-in-from-value

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
   // const BackgroundImage = money;
-  const [amount, setAmount] = useState(100);
+  const [amount, setAmount] = useState(1);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState("");
